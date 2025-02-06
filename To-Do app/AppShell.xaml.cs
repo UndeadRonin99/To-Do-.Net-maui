461a48e479354d7a8f396ae34816064a747db227
UndeadRonin99 <ST10046014@vcconnect.edu.za>
2025-02-07 01:27:42 +0200

    adding project files

---
+++ To-Do app/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace To_Do_app
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ To-Do app/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace To_Do_app
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
